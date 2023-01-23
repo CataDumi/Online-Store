174be1d85d749802a42269acaa27a86f342a9239
CataDumi <cata.dumi.134@gmail.com>
2023-01-23 18:25:13 +0200

    3rd commit - login & register fcn , forms improvement and checking for requirements

---
+++ forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm, Form
+from wtforms import SubmitField, BooleanField, StringField, PasswordField, validators, TextField, TextAreaField, \
+    IntegerField
+from wtforms.validators import DataRequired, URL, InputRequired, NumberRange, Email,Length
+
+
+class EditForm(FlaskForm):
+    name = StringField('Edit book name', validators=[DataRequired()])
+    author = StringField('Edit author name', validators=[DataRequired()])
+    quantity = IntegerField('Edit quantity available ', validators=[DataRequired()])
+    price = IntegerField('Edit book price', validators=[DataRequired()])
+    rating = IntegerField('Edit book rating', validators=[DataRequired(), NumberRange(min=0, max=5)])
+    url = TextField('Edit url', validators=[DataRequired(), URL()])
+    description = TextAreaField('Edit book description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+#Form pt inregistrare users
+class RegisterForm(FlaskForm):
+    name = StringField('Enter name', validators=[DataRequired()])
+    email = TextField('Enter email', validators=[DataRequired(),Email()])
+    password = PasswordField('Enter password', validators=[DataRequired(),Length(min=4)])
+    password_again = PasswordField('Confirm password', validators=[DataRequired(),Length(min=4)])
+    submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    email = TextField('Enter email', validators=[DataRequired(), Email()])
+    password = PasswordField('Enter password', validators=[DataRequired(), Length(min=4)])
+    submit = SubmitField('Submit')
--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import requests.cookies
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm,Form
-from wtforms import StringField, SubmitField,TextField,TextAreaField,IntegerField
-from wtforms import SubmitField, BooleanField, StringField, PasswordField, validators
-from wtforms.validators import DataRequired, URL,InputRequired,NumberRange
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import datetime
+from forms import EditForm, RegisterForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -20,14 +17,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.app_context().push()
 
-# Creez form pt carti
-
-
-# ramane de testat
-
 
 # Creez database pt carti
-
 class Books(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250))
@@ -35,78 +26,114 @@ class Books(db.Model):
     quantity = db.Column(db.Integer)
     price = db.Column(db.Integer)
     rating = db.Column(db.Integer)
+    url = db.Column(db.String(250))
     description = db.Column(db.String(250))
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    email = db.Column(db.String(250))
+    password = db.Column(db.String(250))
+
+
 db.create_all()
 
-# Form pt edit
-class Edit(FlaskForm):
-    name = StringField('Edit book name', validators=[DataRequired(),InputRequired()])
-    author = StringField('Edit author name', validators=[DataRequired()])
-    quantity = IntegerField('Edit quantity available ', validators=[DataRequired()])
-    price = IntegerField('Edit book price', validators=[DataRequired()])
-    rating = IntegerField('Edit book rating', validators=[DataRequired(),validators.NumberRange(min=0,max=5)])
-    description = TextAreaField('Edit book description', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+
+#################################################                         ################################################################
+#################################################                         ################################################################
+#################################################                         ################################################################
 
 @app.route('/', methods=['GET', 'POST'])
 def get_all_posts():
-    if request.method=='GET':
-        preview=db.session.query(Books).all()[:1]  # ca sa vad doar primele cateva carti
-    return render_template("index.html",content=preview,date=datetime.now().strftime("%d/%m/%Y"))
+    if request.method == 'GET':
+        preview = db.session.query(Books).all()[:2]  # ca sa vad doar primele cateva carti
+    return render_template("index.html", content=preview, date=datetime.now().strftime("%d/%m/%Y"))
 
-@app.route('/edit/<int:id>',methods=["GET","POST"])
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    register_user_form = RegisterForm()
+    if request.method == 'POST':
+        if register_user_form.validate_on_submit():
+            new_user = User(name=register_user_form.name.data,
+                            email=register_user_form.email.data,
+                            password=register_user_form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('catalogue'))
+    return render_template('register.html', form=register_user_form)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    login_form=LoginForm()
+    if request.method=="POST":
+        print(login_form.email.data)
+        print(login_form.password.data)
+        if User.query.filter_by(email=login_form.email.data).first():
+            return redirect(url_for('catalogue'))
+        else:
+            return redirect(url_for('register_user'))
+    return render_template('login.html',form=login_form)
+
+
+@app.route('/edit/<int:id>', methods=["GET", "POST"])
 def edit(id):
-    edit_form=Edit()
+    edit_form = EditForm()
 
-    if request.method=='GET':
-    ###aici identific cartea din db si ii preiau caracteristicile pt editare
-        book_to_edit=Books.query.filter_by(id=id).first()
-        print('GET')
-        edit_form.name.data=book_to_edit.name
+    if request.method == 'GET':
+        ###aici identific cartea din db si ii preiau caracteristicile pt editare
+        book_to_edit = Books.query.filter_by(id=id).first()
+        # print('GET')
+        edit_form.name.data = book_to_edit.name
         edit_form.author.data = book_to_edit.author
         edit_form.quantity.data = book_to_edit.quantity
         edit_form.price.data = book_to_edit.price
         edit_form.rating.data = book_to_edit.rating
+        edit_form.url.data = book_to_edit.url
+        print(edit_form.url.data)
         edit_form.description.data = book_to_edit.description
         return render_template('edit.html', form=edit_form)
 
-    #Submit changes to db
-    elif request.method=='POST':
-        print('POST')
-        print(edit_form.name.data)
-        print(edit_form.author.data)
-        print(edit_form.quantity.data)
-        print(edit_form.price.data)
-        print(edit_form.rating.data)
-        print(edit_form.description.data)
+    # Submit changes to db
+    elif request.method == 'POST':
+        if edit_form.validate_on_submit():
+            # print('POST')
+            # print(edit_form.name.data)
+            # print(edit_form.author.data)
+            # print(edit_form.quantity.data)
+            # print(edit_form.price.data)
+            # print(edit_form.rating.data)
+            # print(edit_form.description.data)
 
-        #Enter new data
-        edited_book=Books.query.filter_by(id=id).first()
-        print(edited_book.name)
-        print(edit_form.name.data)
-        edited_book.name=edit_form.name.data
-        edited_book.author=edit_form.author.data
-        edited_book.quantity=edit_form.quantity.data
-        edited_book.price=edit_form.price.data
-        edited_book.rating=edit_form.rating.data
-        edited_book.description=edit_form.description.data
-        db.session.commit()
+            # Enter new data
+            edited_book = Books.query.filter_by(id=id).first()
+            edited_book.name = edit_form.name.data
+            edited_book.author = edit_form.author.data
+            edited_book.quantity = edit_form.quantity.data
+            edited_book.price = edit_form.price.data
+            edited_book.rating = edit_form.rating.data
+            edited_book.url = edit_form.url.data
+            edited_book.description = edit_form.description.data
+            db.session.commit()
+            return redirect(url_for('catalogue'))
+        else:
+            return render_template('edit.html', form=edit_form)
 
-        return redirect(url_for('catalogue'))
 
-
-@app.route('/delete/<int:id>',methods=["GET","POST"])
+@app.route('/delete/<int:id>', methods=["GET", "POST"])
 def delete_book(id):
-    book_to_delete=Books.query.get(id)
+    book_to_delete = Books.query.get(id)
     db.session.delete(book_to_delete)
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
+
 @app.route("/catalogue")
 def catalogue():
     requested_post = None
-    all_books=db.session.query(Books).all()
-    return render_template("catalogue.html",content=all_books,date=datetime.now().strftime("%d/%m/%Y"))
+    all_books = db.session.query(Books).all()
+    return render_template("catalogue.html", content=all_books, date=datetime.now().strftime("%d/%m/%Y"))
 
 
 @app.route('/add_books', methods=['POST', "GET"])
@@ -115,12 +142,13 @@ def add_books():
         print('yas')
 
         # aici vad tot ce se da add din add books
-        print(request.form['name'])
-        print(request.form['author'])
-        print(request.form['quantity'])
-        print(request.form['price'])
-        print(request.form['rating'])
-        print(request.form['description'])
+        # print(request.form['name'])
+        # print(request.form['author'])
+        # print(request.form['quantity'])
+        # print(request.form['price'])
+        # print(request.form['rating'])
+        # print(request.form['url'])
+        # print(request.form['description'])
 
         # aici bag noua carte in db
         new_book = Books(name=request.form['name'],
@@ -128,6 +156,7 @@ def add_books():
                          quantity=request.form['quantity'],
                          price=request.form['price'],
                          rating=request.form['rating'],
+                         url=request.form['url'],
                          description=request.form['description']
                          )
         db.session.add(new_book)
