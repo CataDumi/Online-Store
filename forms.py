ee86e6e54834241cee1d7b761c44494d0f283df5
CataDumi <cata.dumi.134@gmail.com>
2023-02-08 00:36:56 +0200

    9th commit - added search bar func and invoice pdf generator+button

--- forms.py
+++ forms.py
@@ -1,18 +1,18 @@
 from flask_wtf import FlaskForm, Form
 from wtforms import SubmitField, BooleanField, StringField, PasswordField, validators, TextField, TextAreaField, \
-    IntegerField
+    IntegerField,FloatField
 from wtforms.validators import DataRequired, URL, InputRequired, NumberRange, Email,Length
 
 
-class EditForm(FlaskForm):
+class BookForm(FlaskForm):
     name = StringField('Edit book name', validators=[DataRequired()])
     author = StringField('Edit author name', validators=[DataRequired()])
-    quantity = IntegerField('Edit quantity available ', validators=[DataRequired()])
-    price = IntegerField('Edit book price', validators=[DataRequired()])
-    rating = IntegerField('Edit book rating', validators=[DataRequired(), NumberRange(min=0, max=5)])
+    quantity = IntegerField('Edit quantity available ', validators=[DataRequired(message='Error: Integer expected')])
+    price = FloatField('Edit book price', validators=[DataRequired(message='Error: Integer expected')])
+    rating = IntegerField('Edit book rating', validators=[DataRequired(message='Error: Integer expected'), NumberRange(min=0, max=5)])
     url = TextField('Edit url', validators=[DataRequired(), URL()])
     description = TextAreaField('Edit book description', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Submit ')
 
 #Form pt inregistrare users
 class RegisterForm(FlaskForm):
@@ -20,9 +20,9 @@ class RegisterForm(FlaskForm):
     email = TextField('Enter email', validators=[DataRequired(),Email()])
     password = PasswordField('Enter password', validators=[DataRequired(),Length(min=4)])
     password_confirmation = PasswordField('Confirm password', validators=[DataRequired(),Length(min=4)])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Submit registration')
 
 class LoginForm(FlaskForm):
     email = TextField('Enter email', validators=[DataRequired(), Email()])
     password = PasswordField('Enter password', validators=[DataRequired(), Length(min=4)])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Submit login')
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import requests.cookies
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash,send_from_directory
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import datetime
-from forms import EditForm, RegisterForm, LoginForm
+from forms import BookForm, RegisterForm, LoginForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
+from fpdf import FPDF
+from pathlib import Path
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -39,18 +41,19 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(250))
 
 #Table for user cart
-
+class ShoppingCart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_cart=db.Column(db.Integer)
+    book_name=db.Column(db.String(250))
 
 db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-
 @login_manager.user_loader
 def load_user(user_id):
-    # print(f'User id to load: {User.query.get(int(user_id))}')
-
+    print(f'User id to load: {User.query.get(int(user_id))}')
     return User.query.get(int(user_id))
 
 
@@ -62,9 +65,8 @@ def load_user(user_id):
 def get_all_posts():
     if request.method == 'GET':
         preview = db.session.query(Books).all()[:3]  # ca sa vad doar primele cateva carti
-    flash('You were successfully logged in')
     print(f'Logged in user ? {current_user.is_authenticated}')
-    print(f'Current user: {current_user}')
+    print(f'Current user: {current_user},{current_user.name}')
     return render_template("index.html", content=preview, date=datetime.now().strftime("%d/%m/%Y"))
 
 
@@ -129,9 +131,7 @@ def login():
 @app.route('/edit/<int:id>', methods=["GET", "POST"])
 @login_required
 def edit(id):
-    edit_form = EditForm()
-    print('No')
-
+    edit_form = BookForm()
     if request.method == 'GET':
         ###aici identific cartea din db si ii preiau caracteristicile pt editare
         book_to_edit = Books.query.filter_by(id=id).first()
@@ -156,7 +156,7 @@ def edit(id):
             # print(edit_form.rating.data)
             # print(edit_form.description.data)
 
-            # Enter new data
+            # Enter new data in db
             edited_book = Books.query.filter_by(id=id).first()
             edited_book.name = edit_form.name.data
             edited_book.author = edit_form.author.data
@@ -170,6 +170,28 @@ def edit(id):
         else:
             return render_template('edit.html', form=edit_form)
 
+@app.route('/add_books', methods=['POST', "GET"])
+def add_books():
+    add_book_form = BookForm()
+    if request.method == 'POST':
+        # aici bag noua carte in db
+        if add_book_form.validate_on_submit():
+            print('AAAAAAAAAAAAAAAAAAAAAAAAAA')
+            new_book = Books(name=add_book_form.name.data,
+                             author=add_book_form.author.data,
+                             quantity=add_book_form.quantity.data,
+                             price=add_book_form.price.data,
+                             rating=add_book_form.rating.data,
+                             url=add_book_form.url.data,
+                             description=add_book_form.description.data
+                             )
+            db.session.add(new_book)
+            db.session.commit()
+
+            return redirect(url_for('get_all_posts'))
+        return render_template('add_books.html',form=add_book_form)
+    else:
+        return render_template('add_books.html',form=add_book_form)
 
 @app.route('/delete/<int:id>', methods=["GET", "POST"])
 def delete_book(id):
@@ -178,83 +200,153 @@ def delete_book(id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
-
 @app.route("/catalogue")
 def catalogue():
     requested_post = None
     all_books = db.session.query(Books).all()
     return render_template("catalogue.html", content=all_books, date=datetime.now().strftime("%d/%m/%Y"))
 
-
-@app.route('/add_books', methods=['POST', "GET"])
-def add_books():
-    if request.method == 'POST':
-        # aici vad tot ce se da add din add books
-        # print(request.form['name'])
-        # print(request.form['author'])
-        print(f"Quantity attr: {request.form['quantity'], type(request.form['quantity'])}")
-        print(f"Price attr: {request.form['price'], type(request.form['price'])}")
-        print(f"Rating attr:{request.form['rating'],type(request.form['rating'])}")
-        # print(request.form['url'])
-        # print(request.form['description'])
-
-        # aici bag noua carte in db
-        new_book = Books(name=request.form['name'],
-                         author=request.form['author'],
-                         quantity=request.form['quantity'],
-                         price=request.form['price'],
-                         rating=request.form['rating'],
-                         url=request.form['url'],
-                         description=request.form['description']
-                         )
-        db.session.add(new_book)
-        db.session.commit()
-
-        return redirect(url_for('get_all_posts'))
-    else:
-        return render_template('add_books.html')
-
-
 @app.route("/about")
 def about():
     return render_template("about.html")
-
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
+@app.route("/finish order")
+@login_required
+def finish_order():
+    cart = ShoppingCart.query.filter_by(user_cart=int(current_user.id)).all()
+    # when the order is finished, substract the items quantity from the db
+    for item in cart:
+        book=Books.query.filter_by(name=item.book_name).first()
+        book.quantity=book.quantity-1
+        db.session.commit()
 
-@app.route('/logout')
-def logout():
-    logout_user()
+    #when the order is finished, clear the cart
+    print(cart)
+    for item in cart:
+        db.session.delete(item)
+        db.session.commit()
     return redirect(url_for('get_all_posts'))
-
-
-shop_cart = []
-
 
 @app.route('/add_item/<int:id>')
 def add_to_cart(id):
-    print('Item added')
-    print(f'Item id: {id}')
-    item_to_add = Books.query.filter_by(id=id).first()
-    print(item_to_add.name)
-    shop_cart.append(item_to_add)
-    print(f'Cart items: {shop_cart}\t Total: {len(shop_cart)}')
+    print('Item added to cart')
+    if current_user.is_authenticated:
+        lista=ShoppingCart.query.filter_by(user_cart=int(current_user.id)).all()
+        print(f'Lista ta de carti e: {lista,type(lista)}')
+
+    item_to_add=ShoppingCart(user_cart=int(current_user.id),
+                             book_name=Books.query.filter_by(id=id).first().name)
+    db.session.add(item_to_add)
+    db.session.commit()
+
+
     return redirect(url_for('catalogue'))
 
 
 @app.route('/cart')
 @login_required
 def cart_page():
-    print(shop_cart)
+    # get all the titles added by a user , but in a list structure
+    lst = ShoppingCart.query.filter_by(user_cart=int(current_user.id)).all()
+    #search for every title from the list above in the Books table and add them in a list
+    shop_cart = [Books.query.filter_by(name=item.book_name).first() for item in lst]
+
+    print(f'Asta este shopping cart-ul: {shop_cart}')
     total_sum=0
     for item in shop_cart:
         print(item.name)
         total_sum+=item.price
+
     return render_template('cart_page.html',cart=shop_cart,total_sum=total_sum,num_of_products=len(shop_cart))
 
 
+@app.route('/invoice-pdf')
+def invoice_pdf():
+    # get all the titles added by a user , but in a list structure
+    lst = ShoppingCart.query.filter_by(user_cart=int(current_user.id)).all()
+    #search for every title from the list above in the Books table and add them in a list
+    shop_cart = [Books.query.filter_by(name=item.book_name).first() for item in lst]
+
+
+    pdf = FPDF(format='A4', orientation='p', unit='mm')
+
+    # Create the page
+    pdf.add_page()
+    # Header content
+    pdf.set_font(family='Times', size=30, style='B')
+    pdf.cell(w=20, h=12, txt=f'INVOICE ', align='L', ln=1, border=0)
+    pdf.ln(4)
+    pdf.set_font(family='Times', size=16, style='B')
+    pdf.cell(w=20, h=12, txt=f'Date: {datetime.now().strftime("%m/%d/%Y")}', align='L', ln=1, border=0)
+    pdf.set_font(family='Times', size=18, style='I')
+    pdf.cell(w=20, h=12, txt=f'Thank you, {current_user.name.title()}. '
+                             f'We have received your order, and it is really lovely.', align='L', ln=1, border=0)
+    pdf.ln(12)
+
+
+
+    #Columns hdr
+    pdf.set_font(family='Times', size=14, style='B')
+    pdf.cell(w=100, h=12, txt='Book Title', align='C', ln=0, border=1)
+    pdf.cell(w=40, h=12, txt='Book Author', align='C', ln=0, border=1)
+    pdf.cell(w=40, h=12, txt='Book Price (Eur)', align='C', ln=1, border=1)
+
+    total_sum=0
+    for book in shop_cart:
+        # Table content
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=12, txt=book.name, align='C', ln=0, border=1)
+        pdf.cell(w=40, h=12, txt=book.author, align='C', ln=0, border=1)
+        pdf.cell(w=40, h=12, txt=str(book.price), align='C', ln=0, border=1)
+        pdf.ln(12)
+        total_sum+=book.price
+
+    # Adding the total
+    pdf.set_font(family='Times', size=12, style='B')
+    pdf.cell(w=100, h=12, txt='', align='C', ln=0, border=1)
+    pdf.cell(w=40, h=12, txt='', align='C', ln=0, border=1)
+    pdf.cell(w=40, h=12, txt=str(total_sum), align='C', ln=0, border=1)
+    pdf.ln(20)
+    #
+    # Creating the sum sentence
+    pdf.set_font(family='Times', size=18, style='B')
+    pdf.cell(w=0, h=12, txt=f'The total due amont is {total_sum} Eur', align='L', ln=1, border=0)
+
+    # Creating the name of the file
+    pdf.output('static/files/invoice.pdf')
+
+    return send_from_directory(directory=app.static_folder, path='files/invoice.pdf',as_attachment=True)
+
+@app.route('/search', methods=["GET", "POST"])
+def search():
+    if request.method=='POST':
+        print(request.form['searchbox'])
+    search=request.form['searchbox']
+    all_books = db.session.query(Books).all()
+    for book in all_books:
+        if search.lower() in book.name.lower():
+            print('Da, cartea e in lista')
+            result=book
+            break
+        else:
+            result='Searched book is no available'
+    return render_template('search.html',item=result)
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('get_all_posts'))
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
